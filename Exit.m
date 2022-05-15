90a306474e62f60c74a400a70193132d99a4ff6d
yolk-pie-L <91405207+yolk-pie-L@users.noreply.github.com>
2022-05-15 14:49:25 +0800

    Update Exit.m

--- Exit.m
+++ Exit.m
@@ -1,10 +1,10 @@
 function Exit %退出游戏
 beep
-h=dialog('name','请选择','Units','normalized','position',[0.375 0.42 0.25 0.2],'CloseRequestFcn','beep');
+h=dialog('name','请选择','Units','normalized','position',[0.375 0.42 0.25 0.2]);
 uicontrol('parent',h,'style','text','string','是否退出迷宫？',...
     'Units','normalized','position',[0.1 0.6 0.85 0.2],'fontsize',13);
 uicontrol('parent',h,'style','pushbutton','Units','normalized',...
-    'position',[0.15 0.2 0.3 0.2],'string','不好意思点错了','callback','delete(gcbf);');
+    'position',[0.55 0.2 0.3 0.2],'string','否','callback','delete(gcbf);');
 uicontrol('parent',h,'style','pushbutton','Units','normalized',...
-    'position',[0.55 0.2 0.3 0.2],'string','玩够了想歇一会','callback','delete(gcbf),global x,delete(x.f1);clear global x');
+    'position',[0.15 0.2 0.3 0.2],'string','是','callback','delete(gcbf),global x,delete(x.f1);clear global x');
 end
